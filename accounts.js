0b8a28a600905cd99d098ebcb1c51647c72fe26d
Hank <hankhsu8219@gmail.com>
2021-07-19 14:54:03 +0800

    change to AWS MySQL

--- accounts.js
+++ accounts.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "123456",
-    database: "json",
+    host: "aien12four.cxr0m1n24wv8.us-east-1.rds.amazonaws.com",
+    user: "aien12four",
+    password: "uniqlo12aien",
+    database: "images",
     port:3306
     
 });
@@ -15,22 +15,22 @@ var router = express.Router();
 router.route('/id')
 
     // 查筆資源
-    .post(function (req, res)
-    {
+    // .post(function (req, res)
+    // {
 
-        var sql = "SELECT * FROM `images`.`uniqlo` WHERE(`ID` = '"+ req.body.checkmem + "');"
-        console.log(sql)
+    //     var sql = "SELECT * FROM `images`.`uniqlo` WHERE(`ID` = '"+ req.body.checkmem + "');"
+    //     console.log(sql)
 
-        con.query(sql, function (err, result) {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                res.json(result)
-            }
-        })
+    //     con.query(sql, function (err, result) {
+    //         if (err) {
+    //             console.log(err)
+    //         }
+    //         else {
+    //             res.json(result)
+    //         }
+    //     })
         
-    });
+    // });
 
     router.route('/image')
 
@@ -38,7 +38,7 @@ router.route('/id')
     .post(function (req, res)
     {
 
-        var sql = "SELECT * FROM try5 WHERE `clothset` like '%"+req.body.url+"%'"
+        var sql = "SELECT * FROM fashionai WHERE `clothset` like '%"+req.body.url+"%'"
         console.log(sql)
 
         con.query(sql, function (err, result) {
